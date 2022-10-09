f5903a77f838c7eb0c4d6d1dd6d10cfb1fdd81e1
biniruu <biniruu@gmail.com>
2022-10-10 00:00:57 +0900

    feat: implement sending message

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -1,6 +1,25 @@
 import React from 'react'
+import { io } from 'socket.io-client'
+
+const socket = io('http://127.0.0.1:8000')
+console.log('socket', socket)
 
 function Main() {
+  socket.on('connection', () => {
+    console.log('connected🚀')
+  })
+
+  const data = {
+    username: 'biniruu',
+    message: 'Hey you!',
+  }
+
+  socket.emit('sendMessage', data)
+
+  socket.on('disconnected', () => {
+    console.log('disconnected👋🏻')
+  })
+
   return <div>Main</div>
 }
 
